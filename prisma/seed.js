dbe5d07289d4000f59f71b524b024f3c8c2ca4c9
Robert Campbell <Campbell.Rob@protonmail.com>
2020-02-11 12:19:26 -0800

    updated datamodel for prisma, added events schema, seeds. successfully pulling queries

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,5 +1,5 @@
-const { prisma } = require('../apollo/src/generated/prisma-client')
-var faker = require('faker');
+const { prisma } = require("../apollo/src/generated/prisma-client");
+var faker = require("faker");
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -10,7 +10,16 @@ function getRandomInt(min, max) {
 async function main() {
   // Generate a bunch of random users
   for (var i = 0; i < 100; i++) {
-    await prisma.createUser({
+    await prisma.createUser({});
+    await prisma.createProfile({
+      email: "testing email",
+      username: "testing username",
+      firstName: "testing firstName",
+      lastName: "testing lastName",
+      bio: "testing bio",
+      city: "testing city",
+      state: "testing state",
+      phoneNumber: "testing phoneNumber"
     });
   }
 }
